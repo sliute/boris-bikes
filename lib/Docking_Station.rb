275409dd04635f41461d195af9143ef6a561ce6f
Sam Mckay <sammckay12@gmail.com>
2017-01-04 17:56:21 +0000

    Using Instance Variables

--- lib/Docking_Station.rb
+++ lib/Docking_Station.rb
@@ -1,5 +1,15 @@
 class DockingStation
+  attr_reader :bikes
+  
+  def initialize(bikes)
+    @bikes = bikes
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike
+
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,15 +2,17 @@ require "Docking_Station"
 require "Bike"
 
 describe DockingStation do
-  it { is_expected.to respond_to(:release_bike) }
-  # it "responds to release_bike method" do
-  #  expect(DockingStation.new).to respond_to(:release_bike)
-  # end
+#  it { is_expected.to respond_to(:release_bike) }
+  it { should respond_to :dock_bike }
 end
 
-station = DockingStation.new
+station = DockingStation.new(1)
 bike = station.release_bike
 
 describe bike do
   it { is_expected.to respond_to(:working?) }
+end
+
+describe DockingStation.new(1) do
+  it {is_expected.to have_attributes( :bikes => 1) }
 end
