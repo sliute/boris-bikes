72a544e3512b35a088cc981f1d72c79762a8beec
Stefan Liute <stefanliute@gmail.com>
2017-01-04 15:39:14 +0000

    Make Stations get Bikes

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,8 +1,16 @@
 require "Docking_Station"
+require "Bike"
 
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
   # it "responds to release_bike method" do
   #  expect(DockingStation.new).to respond_to(:release_bike)
   # end
+end
+
+station = DockingStation.new
+bike = station.release_bike
+
+describe bike do
+  it { is_expected.to respond_to(:working?) }
 end
